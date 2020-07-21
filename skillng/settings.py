a249ee75da0511fa83ffc45c78b8306b83313de3
JeremAIh <esiebomaj@gmail.com>
2020-07-21 12:44:32 +0100

    started working on authenthication system

--- skillng/settings.py
+++ skillng/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'courses.apps.CoursesConfig'
+    'courses.apps.CoursesConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static/'),]
+
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_URL='/media/'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
